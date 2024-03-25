90ad60b12bbf4fdcd6b183c3b93be44d867ae054
[SW7] 홍소현 <hshyeon86@gmail.com>
2024-03-25 15:14:57 +0900

    qorwns 16953

---
+++ AtoB.js
@@ -0,0 +1,19 @@
+let input = require("fs").readFileSync("example.txt").toString().split(" ");
+let result = Infinity;
+
+let a = Number(input[0]);
+let b = Number(input[1]);
+
+const dfs = (depth, num) => {
+  if (num == b) {
+    result = Math.min(result, depth);
+    return;
+  }
+  if (num > b) return;
+  dfs(depth + 1, num * 2);
+  dfs(depth + 1, Number(num.toString() + 1));
+};
+
+dfs(1, a);
+
+console.log(result == Infinity ? -1 : result);
